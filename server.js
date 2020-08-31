b347f3e69f624f2eae338318fd20b751d48ef924
Zola <zolamcadie@gmail.com>
2020-08-30 22:58:43 -0400

    Add authentication requirement to all routes

--- server.js
+++ server.js
@@ -3,37 +3,28 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 require('dotenv').config()
+const jwt = require('express-jwt')
+const jwksRsa = require('jwks-rsa')
 
 const dataRouter = require('./routers/dataRouter')
-const testRouter = require('./routers/testRouter')
-
-// const mongoSetup = require('./mongoSetup')
-// mongoSetup.connect().then(() => console.log("MongoDB database connection established successfully"))
 
 const PORT = process.env.PORT || 4000
 
-// MongoClient.connect(mongoConnectionString, {
-//   useUnifiedTopology: true
-// }).then(client => {
-//   console.log("MongoDB database connection established successfully")
-//   const db = client.db("demo-db")
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://ceres-demo.us.auth0.com/.well-known/jwks.json`
+  }),
 
-//   app.use(cors())
-//   app.use(bodyParser.json())
-
-//   app.listen(PORT, () => {
-//     console.log("Server is running on port: " + PORT)
-//   })
-
-//   const dataRoutes = dataRouter(db)
-//   app.use('/data', dataRoutes)
-
-//   const testRoutes = testRouter(db)
-//   app.use('/test', testRoutes)
-
-// }).catch(console.error)
+  audience: 'ceres-api',
+  issuer: `https://ceres-demo.us.auth0.com/`,
+  algorithms: ['RS256']
+});
 
 app.use(cors())
+app.use(checkJwt)
 app.use(bodyParser.json())
 
 app.listen(PORT, () => {
@@ -42,6 +33,3 @@ app.listen(PORT, () => {
 
 const dataRoutes = dataRouter()
 app.use('/data', dataRoutes)
-
-// const testRoutes = testRouter()
-// app.use('/test', testRoutes)
